993be6bde22645454bd5d0fed66c9ddf09ae2702
Hiranmoy45 <mathematicsstudent12345@gmail.com>
2023-08-01 09:26:05 +0530

    first oksshiru

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Hero from "@/components/home/Hero"
+
 import Hotel from "@/components/home/Hotel"
 import Travel from "./travel/page.jsx"
 import Footer from "@/components/footer/Footer.jsx"
@@ -8,12 +8,12 @@ import Contact from "./contact/Cont.jsx"
 import AboutUs from "./about/components/Aboutus.jsx"
 
 
-export default function Home() {
+export default function page() {
   return (
    <>
     <div className="main-content w-full px-[var(--margin-x)] pb-8 overflow-hidden">
   <Main/>
- {/* <Hero/> */}
+
  <Hotel/>
  <AboutUs/>
  <Travel/>
