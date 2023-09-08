06dbd4fd90343f7dfc966f2ad08c83b41863303a
Hiranmoy <hiranmoyhs380@gmail.com>
2023-09-08 17:27:47 +0530

    commited by Hiranmoy

--- app/gallary/TravelCard.jsx
+++ app/gallary/TravelCard.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useState , useEffect, useContext} from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 
 
@@ -12,7 +13,13 @@ const TravelCards = ({travelData}) => {
   return (
     <>
      <div className="card">
-    <img className="h-80 w-full rounded-lg object-cover" src={`/images/ProductPhotos/${travelData}`} alt="image" />
+     <Image
+  className="h-80 w-full rounded-lg object-cover"
+  src={`/images/ProductPhotos/${travelData}`}
+  width={800}
+  height={600}
+  alt="image"
+/>
     <div className="absolute inset-0 flex h-full w-full flex-col justify-between">
      
       {/* <div className="rounded-lg bg-gradient-to-t from-[#19213299] via-[#19213266] to-transparent px-4 py-3 pt-14">
