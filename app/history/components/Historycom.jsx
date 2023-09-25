80e0363e286d0c5552ba98ddc65fca45e7be2bce
Hiranmoy <hiranmoyhs380@gmail.com>
2023-09-26 00:16:41 +0530

    first commit

--- app/history/components/Historycom.jsx
+++ app/history/components/Historycom.jsx
@@ -29,7 +29,7 @@ const Historycom = ({sectionsData ,current}) => {
        <section className="text-gray-600 body-font">
       <div className="container px-5  mx-auto">
         <div className="flex flex-col text-center w-full">
-        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">{current || 'Our Client Work Done'}</h1>
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 dark:text-white">{current || 'Our Client Work Done'}</h1>
           <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Our Client Work Done</h2>
         
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base"></p>
